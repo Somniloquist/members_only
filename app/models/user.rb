5b7b427aed978c038a1bf5933f4b5fb088a92034
Robin Unger <robin.e.unger@gmail.com>
2019-09-28 16:45:57 -0500

    generate remember token on user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_create :generate_remember_token
   before_save :downcase_email
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_nil: true
@@ -18,5 +19,10 @@ class User < ApplicationRecord
   private
     def downcase_email
       self.email.downcase!
+    end
+
+    def generate_remember_token
+      remember_token =  SecureRandom.urlsafe_base64.to_s
+      self.remember_token = Digest::SHA1.hexdigest(remember_token)
     end
 end
