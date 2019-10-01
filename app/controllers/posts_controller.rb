4d7ddc3b3e60eab9f19f59f515fbc2b8166195b3
Robin Unger <robin.e.unger@gmail.com>
2019-10-01 15:46:34 -0500

    Create posts and display on index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,18 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.update_attribute(:user_id, current_user.id)
+    if @post.save
+      flash[:success] = "Post successfully created!"
+      redirect_to action: :index
+    else
+      render :new
+    end
   end
 
   def index
+    @posts = Post.all
   end
 
   private
@@ -15,5 +24,9 @@ class PostsController < ApplicationController
         flash[:warning] = "You must be logged in to use this feature."
         redirect_to login_path
       end
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
     end
 end
