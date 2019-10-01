c4cdf9a82bde3596497849fe22c497a879a8fedf
Robin Unger <robin.e.unger@gmail.com>
2019-10-01 16:27:57 -0500

    Validate and test posts

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    user = users(:test_user_one)
+    @post = Post.new(title: "title", body: "body", user: user)
+  end
+
+  test "test post should be valid" do
+    assert @post.valid?
+  end
+
+  test "title should be present" do
+    @post.title = ''
+    assert_not @post.valid?
+  end
+
+  test "body should be present" do
+    @post.body = ''
+    assert_not @post.valid?
+  end
+
+  test "title should not exceed max length" do
+    @post.title = "a" * 256
+    assert_not @post.valid?
+  end
 end
