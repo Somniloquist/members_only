569db8253277fc21a4e72836c96fa82944a609bc
Robin Unger <robin.e.unger@gmail.com>
2019-09-30 22:08:17 -0500

    fix errors with call to current_user method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,13 +3,7 @@ module SessionsHelper
     # provide a new token so it does not get stolen or stale
     remember user
     session[:current_user_id] = user.id
-    @current_user = user
-  end
-
-  def remember(user)
-    remember_token = user.generate_remember_token
-    user.update_attribute(:remember_token, remember_token)
-    cookies.permanent[:remember_token] = remember_token
+    current_user = user
   end
 
   def logout
@@ -19,12 +13,19 @@ module SessionsHelper
   end
 
   def logged_in?
-    !@current_user.nil?
+    !current_user.nil?
   end
 
   def current_user
     @current_user ||= User.find_by(remember_token: cookies[:remember_token])
   end
+
+  def remember(user)
+    remember_token = user.generate_remember_token
+    user.update_attribute(:remember_token, remember_token)
+    cookies.permanent[:remember_token] = remember_token
+  end
+
 
   def current_user=(user)
     @current_user = user
