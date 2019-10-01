5bc70a325efba92ffe84c06ca15a15869110d05f
Robin Unger <robin.e.unger@gmail.com>
2019-09-30 19:22:03 -0500

    Add posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   post    "/login",       to: "sessions#create"
   delete  "/logout",      to: "sessions#destroy"
   resources :users
+  resources :posts, only: [:new, :create, :index]
   resources :sessions, only: [:new, :create, :destroy]
 end
---
+++ test/controllers/posts_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PostsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get new_post_path
+    assert_response :success
+  end
+
+  test "should get index" do
+    get posts_path
+    assert_response :success
+  end
+
+end
